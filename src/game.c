1808232e9f81edfb52910c00bc92dcd5b7e95587
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2020-01-04 23:04:12 +0000

    pawn claim must not include own pieces. fixed

--- src/game.c
+++ src/game.c
@@ -377,7 +377,7 @@ static void calculate_moves(state_s *state)
         /* Add actual taking moves */
         moves |= pawn_takes[player][pos] & (state->occ_a[opponent[player]] | state->en_passant);
         /* Claim for pawns is only taking moves */
-        state->claim[player] |= pawn_takes[player][pos];
+        state->claim[player] |= pawn_takes[player][pos] & ~state->occ_a[player];
       }
       break;
     case ROOK:
