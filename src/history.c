28968000cc2bb0304245cb59346a713ba061465c
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-11 14:47:06 +0000

    COMMENTS

--- src/history.c
+++ src/history.c
@@ -5,21 +5,20 @@
 #include "history.h"
 #include "search.h"
 
+/* Write a new move into the search history */
 void write_search_history(search_job_s *job, int depth, move_s *move)
 {
   memcpy(&job->search_history[depth], move, sizeof(*job->search_history));
 }
 
+/* Simple check for if a move has been made before */
+/* TODO: Transposition table */
 int is_repeated_move(search_job_s *job, int depth, move_s *move)
 {
-  /* Super simple checking for moving back to the square you've
-     just come from */
-  /* TODO: Transposition table */
-  if (depth > 1) {
-    if (move->to == job->search_history[depth-2].from
-	&& move->from == job->search_history[depth-2].to) {
-      return 1;
-    }
+  if (depth > 1 
+      && move->to == job->search_history[depth-2].from
+	    && move->from == job->search_history[depth-2].to) {
+    return 1;
   }
   return 0;
 }
