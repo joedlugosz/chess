c08cab41e87651bff5edcd8189f31899b15f39f6
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-20 13:04:57 +0000

    ADD: ui_info

--- src/commands.c
+++ src/commands.c
@@ -5,6 +5,7 @@
 #include "engine.h"
 #include "fen.h"
 #include "log.h"
+#include "info.h"
 #include "search.h"
 #include "movegen.h"
 #include "io.h"
@@ -29,6 +30,7 @@ void ui_force(engine_s *e) {
   e->mode = ENGINE_FORCE_MODE; 
 }
 
+/* AI to play */
 void ui_go(engine_s *e) {
   e->mode = e->game.turn; 
 }
@@ -192,12 +194,18 @@ void ui_perft(engine_s *e) {
   perft_total(&e->game, depth);
 }
 
+/* Run perft-divide */
 void ui_perftd(engine_s *e) {
   int depth;
   if(!sscanf(get_input(), "%d", &depth)) {
     return;
   }
   perft_divide(&e->game, depth);
+}
+
+/* Print program info */
+void ui_info(engine_s *e) {
+  print_program_info();
 }
 
 /* No Operation */
@@ -211,12 +219,15 @@ void ui_noop_1arg(engine_s *e) {
  *  Command Table
  */
 
+/* Type of command - help display sorts commands by these types */
 typedef enum ui_cmd_type_e {
   CT_GAMECTL,
   CT_DISPLAY,
   CT_XBOARD,
   CT_UNIMP
 } ui_cmd_type_e;
+
+/* Entry in the table of commands */
 typedef struct ui_cmd_s_ {
   ui_cmd_type_e type;
   char cmd[10];
@@ -224,11 +235,49 @@ typedef struct ui_cmd_s_ {
   char desc[100];
 } ui_cmd_s;
 
-enum {
-  N_UI_CMDS = 30
+void ui_help(engine_s *e);
+
+/* Table of commands */
+const ui_cmd_s cmds[] = {
+  { CT_DISPLAY, "attacks",  ui_attacks,    "POS  - Display all pieces that can attack POS" },
+  { CT_XBOARD,  "accepted", ui_accepted,   "     - ???" },
+  { CT_GAMECTL, "black",    ui_black,      "     - AI to play as black" },
+  { CT_XBOARD,  "computer", ui_computer,   "     - ???" },
+  { CT_DISPLAY, "eval",     ui_eval,       "     - Evaluate game" },
+  { CT_GAMECTL, "fen",      ui_fen,        "FEN  - Set the position using a FEN string" },
+  { CT_GAMECTL, "force",    ui_force,      "     - Enter force mode" },
+  { CT_GAMECTL, "getfen",   ui_getfen,     "     - Get the position in FEN notation" },
+  { CT_GAMECTL, "go",       ui_go,         "     - AI to make first move if playing as white" },
+  { CT_GAMECTL, "help",     ui_help,       "     - Display a list of all commands" },
+  { CT_DISPLAY, "info",     ui_info,       "     - Display build information"},
+  { CT_XBOARD,  "level",    ui_level,      "     - ???"},
+  { CT_DISPLAY, "moves",    ui_moves,      "POS  - Display all squares that the piece at POS can move to" },
+  { CT_GAMECTL, "new",      ui_new,        "     - New game" },
+  { CT_XBOARD,  "option",   ui_option,     "     - Set engine option" },
+  { CT_UNIMP,   "otim",     ui_noop_1arg,  "TIME - This function is accepted but currently has no effect" },
+  { CT_GAMECTL, "perft",    ui_perft,      "     - Move generator performance test" },
+  { CT_GAMECTL, "perftd",   ui_perftd,     "     - Move generator performance test, divided by move" },
+  { CT_DISPLAY, "print",    ui_print,      "     - Display the board" },
+  { CT_XBOARD,  "protover", ui_protover,   "PROT - ??? Selects an XBoard protocol of at least PROT" },
+  { CT_GAMECTL, "quit",     ui_quit,       "     - Quit the program" },
+  { CT_GAMECTL, "q",        ui_quit,       "     - Quit the program more quickly" },
+  { CT_UNIMP,   "st",       ui_noop_1arg,  "     - This function is accepted but currently has no effect" },
+  { CT_GAMECTL, "sd",       ui_sd,         "D    - Set the search depth" },
+  { CT_UNIMP,   "time",     ui_noop_1arg,  "     - This function is accepted but currently has no effect" },
+  { CT_UNIMP,   "random",   ui_noop,       "     - This function is accepted but currently has no effect" },
+  { CT_UNIMP,   "result",   ui_result,     "     - This function is accepted but currently has no effect" },
+  { CT_UNIMP,   "undo",     ui_noop,       "     - This function is accepted but currently has no effect" },
+  { CT_UNIMP,   "variant",  ui_noop_1arg,  "     - This function is accepted but currently has no effect" },
+  { CT_GAMECTL, "white",    ui_white,      "     - AI to play as white (enter 'go' to start)" },
+  { CT_GAMECTL, "xboard",   ui_xboard,     "     - Enter XBoard mode" }
 };
 
-const ui_cmd_s cmds[N_UI_CMDS];
+/* Number of commands */
+enum {
+  N_UI_CMDS = sizeof(cmds)/sizeof(*cmds)
+};
+
+/* Help command - display a list of all commands, grouped by type */
 void ui_help(engine_s *e) {
   printf("\n  Command List\n");
   printf("\n   The following commands are used for starting and controlling games:\n");
@@ -263,38 +312,6 @@ void ui_help(engine_s *e) {
   printf("\n");
 }
 
-const ui_cmd_s cmds[N_UI_CMDS] = {
-  { CT_DISPLAY, "attacks",  ui_attacks,    "POS  - Display all pieces that can attack POS" },
-  { CT_XBOARD,  "accepted", ui_accepted,   "     - ???" },
-  { CT_GAMECTL, "black",    ui_black,      "     - AI to play as black" },
-  { CT_XBOARD,  "computer", ui_computer,   "     - ???" },
-  { CT_DISPLAY, "eval",     ui_eval,       "     - Evaluate game" },
-  { CT_GAMECTL, "fen",      ui_fen,        "FEN  - Set the position using a FEN string" },
-  { CT_GAMECTL, "force",    ui_force,      "     - Enter force mode" },
-  { CT_GAMECTL, "getfen",   ui_getfen,     "     - Get the position in FEN notation" },
-  { CT_GAMECTL, "go",       ui_go,         "     - AI to make first move if playing as white" },
-  { CT_GAMECTL, "help",     ui_help,       "     - Display a list of all commands" },
-  { CT_XBOARD,  "level",    ui_level,      "     - ???"},
-  { CT_DISPLAY, "moves",    ui_moves,      "POS  - Display all squares that the piece at POS can move to" },
-  { CT_GAMECTL, "new",      ui_new,        "     - New game" },
-  { CT_XBOARD,  "option",   ui_option,     "     - Set engine option" },
-  { CT_UNIMP,   "otim",     ui_noop_1arg,  "TIME - This function is accepted but currently has no effect" },
-  { CT_GAMECTL, "perft",    ui_perft,      "     - Move generator performance test" },
-  { CT_GAMECTL, "perftd",   ui_perftd,     "     - Move generator performance test, divided by move" },
-  { CT_DISPLAY, "print",    ui_print,      "     - Display the board" },
-  { CT_XBOARD,  "protover", ui_protover,   "PROT - ??? Selects an XBoard protocol of at least PROT" },
-  { CT_GAMECTL, "quit",     ui_quit,       "     - Quit the program" },
-  { CT_GAMECTL, "q",        ui_quit,       "     - Quit the program more quickly" },
-  { CT_UNIMP,   "st",       ui_noop_1arg,  "     - This function is accepted but currently has no effect" },
-  { CT_GAMECTL, "sd",       ui_sd,         "D    - Set the search depth" },
-  { CT_UNIMP,   "time",     ui_noop_1arg,  "     - This function is accepted but currently has no effect" },
-  { CT_UNIMP,   "random",   ui_noop,       "     - This function is accepted but currently has no effect" },
-  { CT_UNIMP,   "result",   ui_result,     "     - This function is accepted but currently has no effect" },
-  { CT_UNIMP,   "undo",     ui_noop,       "     - This function is accepted but currently has no effect" },
-  { CT_UNIMP,   "variant",  ui_noop_1arg,  "     - This function is accepted but currently has no effect" },
-  { CT_GAMECTL, "white",    ui_white,      "     - AI to play as white (enter 'go' to start)" },
-  { CT_GAMECTL, "xboard",   ui_xboard,     "     - Enter XBoard mode" }
-};
 
 int accept_command(engine_s *e, const char *in)
 {
