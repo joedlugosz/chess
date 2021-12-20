cbf35f6a52daa0e531ac620cdda049dd014189d1
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-20 13:06:39 +0000

    ADD: info from version.h

--- src/info.c
+++ src/info.c
@@ -4,14 +4,27 @@
 
 #include "compiler.h"
 #include "os.h"
+#include "version.h"
 #include <stdio.h>
 
+#define str(a) #a
+#define stringify(a) str(a)
+
+const char version[] = GIT_VERSION;
 const char date[] = __DATE__;
-const char version[] = "4.1";
-char compiler[100];
 const char target[] = TARGET_NAME;
-const char os_text[2][10] = { "Linux", "Windows" };
-const char cpu[] = "";
+const char os[] = OS_NAME;
+const char config[] = stringify(CONFIG);
+
+#if defined(__clang__)
+const char compiler[] = "clang " __VERSION__;
+#elif defined(__GNUC__)
+const char compiler[] = "gcc " __VERSION__;
+#elif defined(_MSC_VER)
+const char compiler[] = "MSVC " _MSC_VER;
+#else
+const char compiler[] = "Unknown";
+#endif
 
 enum {
   N_BANNER_LINES = 13,
@@ -26,13 +39,13 @@ const banner_line_s lines[N_BANNER_LINES] = {
   { "",         0                           },
   { "Joe's Chess Engine", 0                 },
   { "",         0                           },
-  { "Version",  "4.1"                       },
+  { "Version",  version                     },
+  { "Config",   config                      },
   { "Built",    date                        },
   { "",         0                           },
   { "Compiler", compiler                    },
   { "Target",   target                      },
-  { "CPU",      cpu                         },
-  { "OS",       os_text[OS]                 },
+  { "OS",       os                          },
   { "",         0                           },
   { "Type 'help' for a list of commands", 0 },
   { "",         0                           }
@@ -41,31 +54,20 @@ const banner_line_s lines[N_BANNER_LINES] = {
 void print_program_info()
 {
   char buf[COL_WIDTH+1];
-#if (COMP==MSVC)
-  sprintf(compiler, "Microsoft Visual C++ %lu", _MSC_FULL_VER);
-#else
-  sprintf(compiler, "%s", COMPILER_NAME);
-# endif
   printf("\n");
   for(int i = 0; i < N_BANNER_LINES; i++) {
-    set_console_black_square();
-    printf("  ");
-    set_console_hilight1();
-    printf("  ");
     if(lines[i].col1[0]) {
       if(lines[i].col2) {
-	sprintf(buf, "%s:", lines[i].col1);
-	printf("%-15s", buf);
-	printf("%-60s", lines[i].col2);
+        sprintf(buf, "%s:", lines[i].col1);
+        printf("%-15s", buf);
+        printf("%-60s", lines[i].col2);
       } else {
-	printf("%-75s", lines[i].col1);
+      	printf("%-75s", lines[i].col1);
       }
     } else {
       printf("%75s", "");
     }
-    set_console_black_square();
     printf("\n");
   }
-  set_console_black_square();
   printf("\n");
 }
