2d8b6c5af215d96eb8182a1d60f8e8c2c68c0fec
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-20 14:16:22 +0000

    ADD: calculate size of array

--- src/info.c
+++ src/info.c
@@ -1,21 +1,25 @@
 /*
- *  Build information
+ *  Program information
  */
 
-#include "compiler.h"
-#include "os.h"
+//#include "compiler.h"
+//#include "os.h"
 #include "version.h"
 #include <stdio.h>
 
 #define str(a) #a
 #define stringify(a) str(a)
 
+/* Defined in version.h */
 const char version[] = GIT_VERSION;
 const char date[] = __DATE__;
 const char target[] = TARGET_NAME;
 const char os[] = OS_NAME;
+
+/* Defined at invocation */
 const char config[] = stringify(CONFIG);
 
+/* Compiler name and version */
 #if defined(__clang__)
 const char compiler[] = "clang " __VERSION__;
 #elif defined(__GNUC__)
@@ -26,16 +30,17 @@ const char compiler[] = "MSVC " _MSC_VER;
 const char compiler[] = "Unknown";
 #endif
 
+/* Program info data */
 enum {
-  N_BANNER_LINES = 13,
   COL_WIDTH = 50
 };
-typedef struct banner_line_s_ {
+
+typedef struct info_line_s_ {
   char col1[COL_WIDTH];
   const char *col2;
-} banner_line_s;
+} info_line_s;
 
-const banner_line_s lines[N_BANNER_LINES] = {
+const info_line_s lines[] = {
   { "",         0                           },
   { "Joe's Chess Engine", 0                 },
   { "",         0                           },
@@ -51,11 +56,12 @@ const banner_line_s lines[N_BANNER_LINES] = {
   { "",         0                           }
 };
 
+/* Prints a program info banner */
 void print_program_info()
 {
   char buf[COL_WIDTH+1];
   printf("\n");
-  for(int i = 0; i < N_BANNER_LINES; i++) {
+  for(int i = 0; i < sizeof(lines)/sizeof(*lines); i++) {
     if(lines[i].col1[0]) {
       if(lines[i].col2) {
         sprintf(buf, "%s:", lines[i].col1);
