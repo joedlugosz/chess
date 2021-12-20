1a6f6fe46a7af7bb03d265e05be203fb4aee0c92
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-20 16:57:57 +0000

    RENAME: key value

--- src/info.c
+++ src/info.c
@@ -34,8 +34,8 @@ enum {
 };
 
 typedef struct info_line_s_ {
-  char col1[COL_WIDTH];
-  const char *col2;
+  char key[COL_WIDTH];
+  const char *value;
 } info_line_s;
 
 const info_line_s lines[] = {
@@ -60,13 +60,13 @@ void print_program_info()
   char buf[COL_WIDTH+1];
   printf("\n");
   for(int i = 0; i < sizeof(lines)/sizeof(*lines); i++) {
-    if(lines[i].col1[0]) {
-      if(lines[i].col2) {
-        sprintf(buf, "%s:", lines[i].col1);
+    if(lines[i].key[0]) {
+      if(lines[i].value) {
+        sprintf(buf, "%s:", lines[i].key);
         printf("%-15s", buf);
-        printf("%-60s", lines[i].col2);
+        printf("%-60s", lines[i].value);
       } else {
-      	printf("%-75s", lines[i].col1);
+        printf("%-75s", lines[i].value);
       }
     } else {
       printf("%75s", "");
