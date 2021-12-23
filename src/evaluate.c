abc3bd033f3bb3a498094fcbbd866e4cb523c877
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-23 19:22:01 +0000

    CHANGE: tagged union option value

--- src/evaluate.c
+++ src/evaluate.c
@@ -30,16 +30,16 @@ int randomness = 0;
 /* All the options can be changed */
 const option_s _eval_opts[] = {
     /* clang-format off */
-  { "Pawn value",            INT_OPT,  &piece_weights[PAWN],    0, 0, 0 },
-  { "Rook value",            INT_OPT,  &piece_weights[ROOK],    0, 0, 0 },
-  { "Bishop value",          INT_OPT,  &piece_weights[BISHOP],  0, 0, 0 },
-  { "Knight value",          INT_OPT,  &piece_weights[KNIGHT],  0, 0, 0 },
-  { "Queen value",           INT_OPT,  &piece_weights[QUEEN],   0, 0, 0 },
-  { "King value",            INT_OPT,  &piece_weights[KING],    0, 0, 0 },
-  { "Mobility bonus",        INT_OPT,  &mobility,               0, 0, 0 },
-  { "Blocked pawn penalty",  INT_OPT,  &blocked,                0, 0, 0 },
-  { "Doubled pawn penalty",  INT_OPT,  &doubled,                0, 0, 0 },
-  { "Randomness",            SPIN_OPT, &randomness,          0, 2000, 0 },
+  { "Pawn value",            INT_OPT,  .value.integer = &piece_weights[PAWN],    0, 0, 0 },
+  { "Rook value",            INT_OPT,  .value.integer = &piece_weights[ROOK],    0, 0, 0 },
+  { "Bishop value",          INT_OPT,  .value.integer = &piece_weights[BISHOP],  0, 0, 0 },
+  { "Knight value",          INT_OPT,  .value.integer = &piece_weights[KNIGHT],  0, 0, 0 },
+  { "Queen value",           INT_OPT,  .value.integer = &piece_weights[QUEEN],   0, 0, 0 },
+  { "King value",            INT_OPT,  .value.integer = &piece_weights[KING],    0, 0, 0 },
+  { "Mobility bonus",        INT_OPT,  .value.integer = &mobility,               0, 0, 0 },
+  { "Blocked pawn penalty",  INT_OPT,  .value.integer = &blocked,                0, 0, 0 },
+  { "Doubled pawn penalty",  INT_OPT,  .value.integer = &doubled,                0, 0, 0 },
+  { "Randomness",            SPIN_OPT, .value.integer = &randomness,          0, 2000, 0 },
     /* clang-format on */
 };
 const options_s eval_opts = {sizeof(_eval_opts) / sizeof(_eval_opts[0]), _eval_opts};
