4ca3cc5e20cc8fc8f3b30aa14dd9ff45cf91d760
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-28 22:52:28 +0000

    ADD: enter_xboard_mode

--- src/ui.h
+++ src/ui.h
@@ -5,6 +5,7 @@
 #ifndef UI_H
 #define UI_H
 
+void enter_xboard_mode(struct engine_s_ *);
 int no_piece_at_square(struct engine_s_ *, square_e);
 int move_is_illegal(struct engine_s_ *, struct move_s_ *);
 
