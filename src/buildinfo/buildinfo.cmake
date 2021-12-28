169f49db66ef2bfa504608a238835a2851d0dc53
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-28 15:21:50 +0000

    CHANGE: supply binary dir to buildinfo.cmake

--- src/buildinfo/buildinfo.cmake
+++ src/buildinfo/buildinfo.cmake
@@ -1,11 +1,16 @@
 # Script to generate build info
 
+# Arguments (required)
+# 0     1  2               3  4
+# cmake -P buildinfo.cmake -- <BINARY DIR>
+set (bin_dir ${CMAKE_ARGV4})
+
 find_package(Git)
 
 # Identify commit
 execute_process(
   COMMAND "${GIT_EXECUTABLE}" rev-parse --verify HEAD --short --dirty
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
   OUTPUT_VARIABLE git_version
   ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
 )
@@ -32,4 +37,4 @@ else ()
 endif ()
 
 # Generate build info source
-configure_file("${CMAKE_SOURCE_DIR}/buildinfo.c.in" "${CMAKE_SOURCE_DIR}/buildinfo.c" @ONLY)
+configure_file("${CMAKE_CURRENT_SOURCE_DIR}/buildinfo.c.in" "${bin_dir}/buildinfo.c" @ONLY)
