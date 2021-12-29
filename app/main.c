35313adc798cfab7fe7dd679d474597c15de024e
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-29 00:51:36 +0000

    ADD: app

---
+++ app/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "engine.h"
+#include "os.h"
+#include "ui.h"
+
+void parse_command_line_args(engine_s *e, int argc, char *argv[]) {
+  for (int arg = 1; arg < argc; arg++) {
+    if (strcmp(argv[arg], "x") == 0) {
+      enter_xboard_mode(e);
+    }
+    if (strcmp(argv[arg], "t") == 0) {
+      /* For testing - like XBoard mode but with Ctrl-C */
+      e->xboard_mode = 1;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  start_session_log();
+  setbuf(stdout, NULL);
+  setup_signal_handlers();
+  init_board();
+
+  /* Genuine(ish) random numbers are used where repeatability is not desirable */
+  srand(clock());
+
+  engine_s engine;
+  init_engine(&engine);
+  parse_command_line_args(&engine, argc, argv);
+  run_engine(&engine);
+
+  return 0;
+}
