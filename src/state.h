a0c3133335308343231aee9411fa3c2c771766bf
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2021-12-31 01:23:55 +0000

    ADD: hash

--- src/state.h
+++ src/state.h
@@ -11,6 +11,8 @@
 #include "debug.h"
 #include "lowlevel.h"
 
+typedef unsigned long long hash_t;
+
 /* Players */
 typedef enum player_e_ { WHITE = 0, BLACK, N_PLAYERS } player_e;
 
@@ -36,6 +38,9 @@ typedef enum {
 } square_e;
 
 /* clang-format on */
+
+/* Number of ranks (horizontal) and files (vertical) on the board */
+enum { N_RANKS = 8, N_FILES = 8 };
 
 /* Sides of the board */
 typedef enum boardside_e { QUEENSIDE = 0, KINGSIDE, BOTHSIDES, N_BOARDSIDE } boardside_e;
@@ -105,6 +110,8 @@ typedef struct state_s_ {
   status_t check[N_PLAYERS]; /* Whether each player is in check */
   castle_rights_t castling_rights;
   bitboard_t en_passant; /* En-passant squares */
+  hash_t hash;
+  int ply;
 } state_s;
 
 /* move_s holds the game state as well as info about moves */
@@ -137,12 +144,17 @@ void setup_board(state_s *, const piece_e *, player_e, castle_rights_t, bitboard
 
 bitboard_t get_attacks(state_s *state, square_e target, player_e attacking);
 void make_move(state_s *state, move_s *move);
+<<<<<<< HEAD
 static inline void change_player(state_s *state) { state->turn = opponent[state->turn]; }
 int check_legality(state_s *state, move_s *move);
 static inline int move_equal(move_s *move1, move_s *move2) {
   return (move1 && move2 && move1->from == move2->from && move1->to == move2->to &&
           move1->promotion == move2->promotion);
 }
+=======
+void change_player(state_s *state);
+int check_legality(state_s *state, move_s *move);
+>>>>>>> ADD: hash
 
 static inline int is_valid_square(square_e square) { return (square >= 0 && square < N_SQUARES); }
 static inline square_e bit2square(bitboard_t mask) {
