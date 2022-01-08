8d97a98924b4434aa22d622108c4b498399b9cc4
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2022-01-08 12:29:28 +0000

    FORMATTING

--- src/posix.c
+++ src/posix.c
@@ -43,9 +43,7 @@ void ignore_sigint(void) { signal(SIGINT, SIG_IGN); }
  */
 
 /* Check whether an input FILE is a terminal or a file */
-int is_terminal(FILE *f) {
-  return isatty(fileno(f));
-}
+int is_terminal(FILE *f) { return isatty(fileno(f)); }
 
 void set_console_hilight1(void) { printf("%s", HLITE1_SQUARE); }
 
