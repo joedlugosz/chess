03dd19c48ea0ceda4a8d933b678b39a8db5d2971
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2022-02-01 20:26:03 +0000

    ADD: ASSERTS logic

--- src/debug.h
+++ src/debug.h
@@ -22,32 +22,39 @@ extern log_s xboard_log;
 extern log_s think_log;
 extern log_s error_log;
 
+#ifdef ASSERTS
+
 void assert_fail(log_s *log, const char *file, const char *func, const int line,
                  const char *condition);
 
-#ifdef LOGGING
-#  define LOG_THOUGHT(c, d, s, a, b) log_thought(&think_log, c, d, s, a, b)
+#  ifdef LOGGING
+#    define ASSERT(x)                                              \
+      if (!(x)) {                                                  \
+        assert_fail(&error_log, __FILE__, __func__, __LINE__, #x); \
+      }
+
+#  else
+#    define ASSERT(x)                                     \
+      if (!(x)) {                                         \
+        assert_fail(0, __FILE__, __func__, __LINE__, #x); \
+      }
+
+#  endif
 #else
-#  define LOG_THOUGHT(c, d, s, a, b)
+#  define ASSERT(x)
 #endif
 
 #ifdef LOGGING
 void start_log(log_s *log, newevery_e new_log, const char *fmt, ...);
 void print_log(log_s *log, const char *fmt, ...);
-#  define ASSERT(x)                                              \
-    if (!(x)) {                                                  \
-      assert_fail(&error_log, __FILE__, __func__, __LINE__, #x); \
-    }
+#  define LOG_THOUGHT(c, d, s, a, b) log_thought(&think_log, c, d, s, a, b)
 #  define START_LOG(l, n, f, ...) start_log(l, n, f, __VA_ARGS__)
 #  define PRINT_LOG(l, f, ...) print_log(l, f, __VA_ARGS__)
 
 void debug_thought(FILE *f, search_job_s *job, int depth, score_t score, score_t alpha,
                    score_t beta);
 #else
-#  define ASSERT(x)                                     \
-    if (!(x)) {                                         \
-      assert_fail(0, __FILE__, __func__, __LINE__, #x); \
-    }
+#  define LOG_THOUGHT(c, d, s, a, b)
 #  define START_LOG(l, n, f, ...)
 #  define PRINT_LOG(l, f, ...)
 #endif /* LOGGING */
