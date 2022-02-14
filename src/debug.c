9ba63a8fa24aa482d926857c2e7ca29eccceece5
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2022-02-14 22:45:13 +0000

    COMMENTS

--- src/debug.c
+++ src/debug.c
@@ -1,6 +1,6 @@
-// /*
-//  *  Debug logging
-//  */
+/*
+ *  Debug logging
+ */
 
 #include "debug.h"
 
@@ -16,6 +16,7 @@
 #include "os.h"
 #include "search.h"
 
+/* Report an error to stderror and as an xboard message to stdout */
 void report_error(const char *src_file, const char *func, const int line, const char *msg1,
                   const char *msg2) {
   fprintf(stderr, "Process: %d\n", get_process_id());
@@ -29,14 +30,14 @@ void report_error(const char *src_file, const char *func, const int line, const 
   print_backtrace();
 }
 
+/* Called when ASSERT() fails. Resign and abort */
 void assert_fail(const char *src_file, const char *func, const int line, const char *condition) {
   report_error(src_file, func, line, "Debug assertation failed: ", condition);
   printf("resign\n");
   abort();
 }
 
-/* Thought logging */
-
+/* Print alpha, beta, search history */
 void debug_thought(search_job_s *job, int depth, score_t score, score_t alpha, score_t beta) {
   printf("\n%2d %10d ", depth, job->result.n_leaf);
   if (alpha > -100000)
