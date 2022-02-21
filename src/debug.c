518990f934f40a9a91345250d22263c8cbd3ae4a
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2022-02-21 15:01:23 +0000

    ADD: logging to file

--- src/debug.c
+++ src/debug.c
@@ -14,12 +14,53 @@
 #include "search.h"
 #include "state.h"
 
+FILE *logfile;
+
+#define USE_LOGFILE 1
+#define LOGFILE_PID 0
+
+void debug_init() {
+  if (USE_LOGFILE) {
+    char filename[100];
+    if (LOGFILE_PID) {
+      int pid = get_process_id();
+      sprintf(filename, "%d.log", pid);
+    } else {
+      sprintf(filename, "chess.log");
+    }
+    logfile = fopen(filename, "a");
+    setbuf(logfile, NULL);
+    if (!logfile) {
+      perror("debug_init:");
+      exit(1);
+    }
+  } else {
+    logfile = stdout;
+  }
+}
+
+void debug_print(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  if (logfile) {
+    vfprintf(logfile, fmt, args);
+  }
+}
+
+void debug_exit() {
+  if (logfile && logfile != stdout) {
+    fclose(logfile);
+  }
+}
+
 /* Report an error to stderror and as an xboard message to stdout */
 void report_error(const char *src_file, const char *func, const int line, const char *msg1,
                   const char *msg2) {
-  fprintf(stderr, "Process: %d\n", get_process_id());
-  fprintf(stderr, "At: %s %s:%d\n", src_file, func, line);
-  fprintf(stderr, "%s %s\n", msg1, msg2);
+  if (logfile != stdout) {
+    fprintf(stderr, "Process: %d\n", get_process_id());
+    fprintf(stderr, "At: %s %s:%d\n", src_file, func, line);
+    fprintf(stderr, "%s %s\n", msg1, msg2);
+  }
 
   printf("{ Process: %d }\n", get_process_id());
   printf("{ At: %s %s:%d }\n", src_file, func, line);
@@ -38,15 +79,15 @@ void assert_fail(const char *src_file, const char *func, const int line, const c
 /* Print alpha, beta, search history */
 void debug_thought(struct search_job_s_ *job, int depth, score_t score, score_t alpha,
                    score_t beta) {
-  printf("%2d %10d ", depth, job->result.n_leaf);
+  fprintf(logfile, "%2d %10d ", depth, job->result.n_leaf);
   if (alpha > -100000)
-    printf("%7d ", alpha);
+    fprintf(logfile, "%7d ", alpha);
   else
-    printf("     -B ");
+    fprintf(logfile, "     -B ");
   if (beta < 100000)
-    printf("%7d ", beta);
+    fprintf(logfile, "%7d ", beta);
   else
-    printf("     +B ");
-  print_thought_moves(depth, job->search_history);
-  printf("\n");
+    fprintf(logfile, "     +B ");
+  print_thought_moves(logfile, depth, job->search_history);
+  fprintf(logfile, "\n");
 }
