96dfcbd9c152b88b230d50d6086a3d13bf1981fb
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2022-02-23 18:27:03 +0000

    ADD: PV stack

--- src/io.h
+++ src/io.h
@@ -13,11 +13,13 @@
 #include "search.h"
 #include "state.h"
 
+struct pv;
+
 void print_board(state_s *state, bitboard_t hl1, bitboard_t hl2);
 void print_plane(bitboard_t plane, bitboard_t indicator);
 void print_move(move_s *move);
 void print_plane_rank(unsigned char rank, unsigned char indicator);
-void print_thought_moves(FILE *logfile, int depth, move_s *moves);
+void print_pv(FILE *f, struct pv *pv);
 const char *get_input(void);
 void get_input_to_buf(char *buf, size_t buf_size);
 const char *get_delim(char delim);
@@ -25,6 +27,7 @@ int parse_square(const char *, square_e *);
 int parse_move(const char *, move_s *);
 int format_square(char *, square_e);
 int format_move(char *, move_s *, int);
-void xboard_thought(search_job_s *job, int depth, score_t score, clock_t time, int nodes);
+void xboard_thought(search_job_s *job, struct pv *pv, int depth, score_t score, clock_t time,
+                    int nodes);
 
 #endif /* IO_H */
