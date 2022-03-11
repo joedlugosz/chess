0418d2d5358f254f1dc7afacaec9929bf8d015a4
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2022-03-11 20:57:10 +0000

    FIX: pedantic warning

--- src/info.c
+++ src/info.c
@@ -35,7 +35,7 @@ const info_line_s lines[] = {
 
 /* Prints a program info banner */
 void print_program_info() {
-  char buf[COL_WIDTH * sizeof(lines)];
+  char buf[1000];
   printf("\n");
   for (int i = 0; i < sizeof(lines) / sizeof(*lines); i++) {
     if (lines[i].key[0]) {
