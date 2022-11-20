0ca14d539b0e3415438189a9f32655ac5a5eef9b
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2022-11-20 11:50:26 +0000

    Fix: No version string displayed on info banner

--- src/buildinfo/buildinfo.cmake
+++ src/buildinfo/buildinfo.cmake
@@ -9,11 +9,24 @@ find_package(Git QUIET)
 if (Git_FOUND)
   # Identify version from last tag and commit
   execute_process(
-    COMMAND "${GIT_EXECUTABLE}" describe --dirty
+    COMMAND "${GIT_EXECUTABLE}" describe --tags --abbrev=0
     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-    OUTPUT_VARIABLE git_version
+    OUTPUT_VARIABLE git_version_tag
     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
   )
+  execute_process(
+    COMMAND "${GIT_EXECUTABLE}" rev-parse --abbrev-ref HEAD
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    OUTPUT_VARIABLE git_branch_name
+    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  execute_process(
+    COMMAND "${GIT_EXECUTABLE}" rev-parse --short HEAD
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    OUTPUT_VARIABLE git_commit_short
+    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  set (git_version "${git_version_tag}-${git_branch_name}-${git_commit_short}")
 else ()
   set (git_version "Unknown")
 endif ()
