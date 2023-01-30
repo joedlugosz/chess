e85327eedb527dcf071aabc843370ffb22e218e7
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2023-01-30 21:03:21 +0000

    Add definitions for find_magics.c

--- src/moves.h
+++ src/moves.h
@@ -4,9 +4,16 @@
 #ifndef MOVES_H
 #define MOVES_H
 
-struct position;
+#include "position.h"
 
 void calculate_moves(struct position *position);
 void check_magics();
+
+/* These functions are used by find_magics.c */
+bitboard_t unpack_moves(bitboard_t template, bitboard_t packed);
+typedef bitboard_t (*generate_blocker_fn)(enum square);
+typedef bitboard_t (*generate_moves_fn)(enum square, bitboard_t);
+bitboard_t generate_bishop_moves(enum square square, bitboard_t blockers);
+bitboard_t generate_rook_moves(enum square square, bitboard_t blockers);
 
 #endif
