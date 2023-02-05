0ef9f47490685beaf78e80895dd15a1aa3140471
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2023-02-05 10:41:08 +0000

    Add cases for uncovered attacks

--- test/src/see.c
+++ test/src/see.c
@@ -26,6 +26,28 @@ int test_see() {
               "defended by queen where there is also a white knight to take "
               "the queen.");
 
+  load_fen(&position, "rnbqkb1r/ppppp1pp/8/5p2/6n1/1P2P3/P1PPBPPP/RNB1K1NR",
+           "w", "KQkq", "f6", "0", "4");
+  TEST_ASSERT(see_after_move(&position, E2, G4, BISHOP) == 0,
+              "*Ng4 bxg4 (+3) *pxg4 (-3) -> 0");
+
+  load_fen(&position, "rnbqkb1r/ppppp1pp/8/5p2/6n1/1P2P3/P1PPBPPP/RNBQK1NR",
+           "w", "KQkq", "f6", "0", "4");
+  TEST_ASSERT(see_after_move(&position, E2, G4, BISHOP) == 1,
+              "*Ng4 bxg4 (+3) *fxg4 (-3) Qxg4 (+1) -> 1");
+
+  load_fen(&position, "rnb1kb1r/pppp2pp/4p3/5pq1/6n1/1P2PP2/PBPPB1PP/RN1QK1NR",
+           "w", "KQkq", "f6", "0", "4");
+  TEST_ASSERT(
+      see_after_move(&position, F3, G4, PAWN) == 3,
+      "*Ng4 fxg4 (+3) *fxg4 (-1) bxg4 (+1) (*queen does not retaliate) -> 3");
+
+  load_fen(&position,
+           "rnb2b1r/pppp2pp/4p3/5pqk/P2P2n1/1PN1PP2/1BP1B1PP/R2QK1NR", "w",
+           "KQkq", "-", "1", "9");
+  TEST_ASSERT(
+      see_after_move(&position, F3, G4, PAWN) == 3,
+      "*Ng4 fxg4 (+3) *fxg4 (-1) bxg4 (+1) (*queen does not retaliate) -> 3");
   return 0;
 }
 
