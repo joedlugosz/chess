566ddbf73d9cef6a1bb63066e9edac30ed5dc183
Joe Dlugosz <jdlugosz@hotmail.co.uk>
2023-02-11 13:16:05 +0000

    Include time_margin in job.stop_time

--- src/search.c
+++ src/search.c
@@ -378,7 +378,7 @@ void search(int target_depth, double time_budget, double time_margin,
     /* Search based on `time_budget` */
     min = MIN_ITERATION_DEPTH;
     max = MAX_ITERATION_DEPTH + 1;
-    job.stop_time = job.start_time + time_budget - 0.01;
+    job.stop_time = job.start_time + time_budget * (1.0 + time_margin) - 0.01;
   } else if (target_depth < MIN_ITERATION_DEPTH) {
     /* Fixed-depth shallow search without iterative deepening */
     min = target_depth;
@@ -388,7 +388,7 @@ void search(int target_depth, double time_budget, double time_margin,
     /* Fixed depth deep search with iterative deepening */
     min = MIN_ITERATION_DEPTH;
     max = target_depth + 1;
-    job.stop_time = job.start_time + time_budget - 0.01;
+    job.stop_time = job.start_time + time_budget * (1.0 + time_margin) - 0.01;
   }
 
   for (int depth = min; depth < max; depth++) {
